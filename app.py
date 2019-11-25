3c45cb31bd0ce950ac2ff179a615a48a70854377
Leticia <gl.leticia@gmail.com>
2019-11-25 13:36:21 +0100

    correcting heroku problems3

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ app.secret_key = os.urandom(24)
 app.config['MONGO_URI'] = os.environ.get("MONGODB_URI")
 #app.config.from_object(Config)
 #mongo = PyMongo(app)
-#client = pymongo.MongoClient(os.getenv('MONGO_URI'))
-#db = client.cookbook
+
 
 app.config["MONGO_DBNAME"] = 'didaccookbook'
-app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
+#app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
 client = pymongo.MongoClient(os.getenv('MONGO_URI'))
 db = client.didaccookbook
 
